3570deed6482397c3b180ff99bc039d78b49d92e
kobkob2018 <yacov.avr@gmail.com>
2023-03-28 14:00:17 +0300

    logo link

--- sites/views/site_header/site_logo.php
+++ sites/views/site_header/site_logo.php
@@ -1,1 +1,3 @@
-<img src="<?= $this->file_url_of('logo',$this->data['site']['logo']) ?>" alt="<?= $this->data['site']['title']; ?>" />
+<a href="<?= outer_url() ?>" title = "<?= $this->data['site']['title'] ?>">
+    <img src="<?= $this->file_url_of('logo',$this->data['site']['logo']) ?>" alt="<?= $this->data['site']['title']; ?>" />
+</a>
