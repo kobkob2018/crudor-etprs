3136db44072bd1498c8354cceae46f6ad4d7c7c5
kobkob2018 <yacov.avr@gmail.com>
2023-04-17 14:30:48 +0300

    ok

--- myleads/controllers/yaad_return_controller.php
+++ myleads/controllers/yaad_return_controller.php
@@ -37,7 +37,7 @@
 		}
 
         protected function init_handler_module($cc_log,$success = 'error'){
-           
+           exit("herreeee");
             if($cc_log['handle_module'] != '' && $cc_log['handle_method'] != ''){
 
                 $action_data = array(
@@ -48,10 +48,10 @@
                 return $this->call_module($cc_log['handle_module'],$cc_log['handle_method'],$action_data);
             }
             if($success == 'ok'){
-                SystemMessages::add_success_message($cc_log['details']." - התשלום בוצע בהצלחה");
+              //  SystemMessages::add_success_message($cc_log['details']." - התשלום בוצע בהצלחה");
             }
             else{
-                SystemMessages::add_err_message($cc_log['details']." - אירעה שגיאה בעת התשלום, אנא נסה שוב.");
+               // SystemMessages::add_err_message($cc_log['details']." - אירעה שגיאה בעת התשלום, אנא נסה שוב.");
             }
         }
 
