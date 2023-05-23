d36545d64562f75be5cd7189f98e429afc871edf
kobkob2018 <yacov.avr@gmail.com>
2023-05-23 13:27:08 +0300

    ok

--- admin/views/content_pages/helper.php
+++ admin/views/content_pages/helper.php
@@ -24,6 +24,21 @@
             <input class="code-input-inline" type="text" value="go-left-bottom" />
         </div>
 
+
+        <div class = "helper-code">
+             מודול הצעות מחיר<br/>
+            
+            סגור: <input class="code-input" type="text" value="{{% mod | quotes | print_cat | cat_id:1 %}}" />
+            <br/>
+            פתוח: <input class="code-input" type="text" value="{{% mod | quotes | print_cat | cat_id:1 state:open %}}" />
+            <br/>
+            יש לשנות את המספר 1 למספר הקטגוריה הרצוייה
+            <br/>
+            על מנת למנוע מעטפת מיותרת, מומלץ לשלב עם תגית עיצוב<br/>
+            <input class="code-input-inline" type="text" value="nowrap" />
+        </div>
+
+        
     </div>
 
 
@@ -44,6 +59,31 @@
         </div>
 
         <div class="helper-code">
+            <input class="code-input-inline" type="text" value="nowrap" />
+            <br/>
+            אם רוצים לא לעטוף אלמנט בכלל. כשאין צורך בשום עיצוב זה חוסך שכבת HTML וגוגל אוהבים את זה
+        </div>
+        <div class="helper-code">
+            <input class="code-input-inline" type="text" value="kova" />
+            <br/>
+            קובייה אלגנטית עם כובע עגול
+            <br/>
+            <input class="code-input-inline" type="text" value="kova-b" />
+            <br/>
+            קובייה אלגנטית עם כובע עגול בצבע מודגש
+            <b>ניתן לשלב עם תגיות h-r, h-l לתוצאה נהדרת</b>
+        </div>
+        <div class="helper-code">
+            <input class="code-input-inline" type="text" value="h-r" />
+            <br/>
+            <input class="code-input-inline" type="text" value="h-l" />
+            <br/>
+            חלוקת קוביות לימין ושמאל
+            <b>ניתן לשלב עם תגיות kova, kova-b לתוצאה נהדרת</b>
+            <br/>
+            <b>חשוב לשים אותן ביחד אחת ליד השנייה ולפי הסדר על מנת לשמור על רציפות הדף</b>
+        </div>
+        <div class="helper-code">
             <input class="code-input-inline" type="text" value="go-left" />
             <br/>
             <input class="code-input-inline" type="text" value="go-left-top" />
@@ -53,6 +93,11 @@
             <input class="code-input-inline" type="text" value="go-left-bottom" />
             <br/>
             כך ניתן לשלוח בלוק לעמודה השמאלית, למעלה, באמצע או למטה בהתאמה
+            <br/>
+            
+            <input class="code-input-inline" type="text" value="go-right" />
+            <br/>
+            שליחה לעמודה ימנית
         </div>
     </div>
 </div>
