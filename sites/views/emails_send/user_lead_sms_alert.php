b0f0c05d02ea46abb05f30f828f881c4050334d1
kobkob2018 <yacov.avr@gmail.com>
2023-06-15 11:43:06 +0300

    ok

--- sites/views/emails_send/user_lead_sms_alert.php
+++ sites/views/emails_send/user_lead_sms_alert.php
@@ -1,1 +1,7 @@
-פנייה משירות 10 <?= $info['lead']['full_name'] ?>, <?= $info['lead']['city_name'] ?> <?= $info['lead']['phone'] ?>
+לידים משירות 10: 
+<?= $info['lead']['full_name'] ?>, 
+<?= $info['lead']['city_name'] ?>, 
+<?= $info['lead']['phone'] ?>, 
+<?= $info['lead']['cat_tree_name'] ?>, 
+
+<?= $info['auth_link'] ?>
