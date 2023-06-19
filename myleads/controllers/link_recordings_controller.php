6479929e122588f948bfab95cea64b886d6f7a21
kobkob2018 <yacov.avr@gmail.com>
2023-06-19 12:54:18 +0300

    ok

--- myleads/controllers/link_recordings_controller.php
+++ myleads/controllers/link_recordings_controller.php
@@ -7,7 +7,9 @@
         $link_records_url = get_config("link_records_url");
         $outputName = $link_records_url.$_GET['filename'].".mp3";
         header('Content-Disposition: attachment; filename="call_recording.mp3"');
-        header("Content-Type: audio/mpeg3");
+        
+        //this line not working anymore...
+        //header("Content-Type: audio/mpeg3");
         header("Content-Length: " . filesize($outputName));
         echo (file_get_contents($outputName));
         unlink($outputName);
