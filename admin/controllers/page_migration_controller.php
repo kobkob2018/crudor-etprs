1434e0df9b524a8757d1c149ee05298c16396c73
kobkob2018 <yacov.avr@gmail.com>
2023-07-03 14:16:14 +0300

    ok

---
+++ admin/controllers/page_migration_controller.php
@@ -0,0 +1,122 @@
+<?php
+  class Page_migrationController extends CrudController{
+    public $add_models = array("sites","site_migration", "page_migration");
+
+    public function list(){
+        //if(session__isset())
+        $filter_arr = $this->get_base_filter();
+        $site_migration = Site_migration::find($filter_arr);      
+        $this->data['site_migration'] = $site_migration;
+        if(empty($site_migration)){
+            return $this->redirect_to(inner_url("site_migration/add/"));
+        }
+        else{
+            $site_migration_id = $site_migration[0]['id'];
+            return $this->redirect_to(inner_url("site_migration/edit/?row_id=$site_migration_id"));
+        }
+    }
+
+    protected function get_base_filter(){
+
+        $filter_arr = array(
+            'site_id'=>$this->data['work_on_site']['id']
+        );  
+        return $filter_arr;     
+    }
+
+    public function edit(){
+        return parent::edit();
+    }
+
+    public function updateSend(){
+        return parent::updateSend();
+    }
+
+    public function add(){
+        return parent::add();
+    }       
+
+    public function createSend(){
+        return parent::createSend();
+    }
+
+    public function delete(){
+        return parent::delete();      
+    }
+
+
+    public function include_edit_view(){
+        $this->include_view('site_migration/edit.php');
+    }
+
+    public function include_add_view(){
+        $this->include_view('site_migration/add.php');
+    }   
+
+    protected function update_success_message(){
+        SystemMessages::add_success_message("התיאום עודכן בהצלחה");
+
+    }
+
+    protected function create_success_message(){
+        SystemMessages::add_success_message("התיאום נוצר בהצלחה");
+
+    }
+
+    protected function delete_success_message(){
+        SystemMessages::add_success_message("התיאום נמחק");
+    }
+
+    protected function row_error_message(){
+      SystemMessages::add_err_message("לא נבחר תיאום");
+    }   
+
+    protected function delete_item($row_id){
+      return Site_migration::delete($row_id);
+    }
+
+    protected function get_item_info($row_id){
+      return Site_migration::get_by_id($row_id);
+    }
+
+    public function eject_url(){
+      return inner_url('site_migration/list/');
+    }
+
+    public function url_back_to_item($item_info){
+      return inner_url("site_migration/list/?row_id=".$item_info['id']);
+    }
+
+    protected function get_fields_collection(){
+      return Site_migration::setup_field_collection();
+    }
+
+    protected function update_item($item_id,$update_values){
+      return Site_migration::update($item_id,$update_values);
+    }
+
+    protected function get_priority_space($filter_arr, $item_to_id){
+        return Site_migration::get_priority_space($filter_arr, $item_to_id);
+      }
+
+    protected function create_item($fixed_values){
+        $work_on_site = Sites::get_user_workon_site();
+        $site_id = $work_on_site['id'];
+
+        $fixed_values['site_id'] = $site_id;
+        $old_domain = $fixed_values['old_domain'];
+        $old_site_data = Site_migration::get_old_site_data_by_domain($old_domain);
+        if(!$old_site_data){
+            SystemMessages::add_err_message("לא נמצא אתר עם הדומיין שציינת במערכת הישנה");
+            $this->eject_redirect();
+            return false;
+        }
+        $fixed_values['old_unk'] = $old_site_data['unk'];
+        $fixed_values['old_id'] = $old_site_data['site_id'];
+        $fixed_values['old_title'] = $old_site_data['title'];
+        
+        return Site_migration::create($fixed_values);
+    }
+
+  }
+?>
---
+++ admin/models/page_migration_model.php
@@ -0,0 +1,57 @@
+<?php
+  class Page_migration extends TableModel{
+
+    private static $ilbiz_db = NULL;
+
+    protected static $main_table = 'page_migration';
+
+    protected static function getIlbizDb() {
+        if (!isset(self::$ilbiz_db)) {
+          $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+          self::$ilbiz_db = new PDO('mysql:host='.get_config('ilbiz_db_host').'; port='.get_config('ilbiz_db_port').'; dbname='.get_config('ilbiz_db_db').'', get_config('ilbiz_db_user'), get_config('ilbiz_db_pass'), $pdo_options);      
+          }
+        return self::$ilbiz_db;
+    }
+
+    public static function get_old_site_page_list($site_migration){
+
+
+        $ilbiz_db = self::getIlbizDb();
+        $sql = "select * from content_pages WHERE unk = :unk";
+        $req = $ilbiz_db->prepare($sql);
+        $req->execute(array('domain'=>$domain));
+        $result = $req->fetchAll();
+        $content_pages = array();
+        $formated_params = array(
+            'name', 'content', 'keywords', 'description', 'redirect_301'
+        );
+        if($result){
+            foreach($result as $content_page){
+                foreach($formated_params as $param)
+                $content_page[$param] = utgt($content_page[$param]);
+                $content_pages[] = $content_page
+            }
+        }
+
+        $migrated_pages = self::get_list(array($site_id=>$site_migration['site_id']));
+
+        $migrated_pages_indexed = Helper::eazy_index_arr_by('old_page_id',$migrated_pages);
+
+        foreach($content_pages as $key=>$content_page){
+            $migrated_page = array(
+                'migrated'=>"no",
+                'id'=>''
+                'page_id'=>'',
+                'version'=>'',
+                'has_form'=>'no',
+            );
+            if(isset($migrated_pages_indexed[$content_page['id']])){
+                $migrated_page = $migrated_pages_indexed[$content_page['id']];
+            }
+            $content_pages[$key]['migrated_page'] = $migrated_page;
+        }
+
+        return $content_pages;
+    } 
+}
+?>
