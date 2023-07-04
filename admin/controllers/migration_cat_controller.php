70ae7b0270f8dc58497fbdbd6c332539da7fbc6f
kobkob2018 <yacov.avr@gmail.com>
2023-07-04 16:24:10 +0300

    ok

--- admin/controllers/migration_cat_controller.php
+++ admin/controllers/migration_cat_controller.php
@@ -6,13 +6,9 @@
         //if(session__isset())
 
         $migration_cat_list = Migration_cat::get_old_cat_tree();
+        
+        $this->data['migrate_cat_list'] = $migration_cat_list;
         return $this->include_view("migration_cat/list.php");
-        return;
-        $migration_site_id = $migration_site['id'];
-        $migrate_page_list = Migration_page::get_old_site_page_list($migration_site);
-        foreach($migrate_page_list as $migrate_page){
-            print_help($migrate_page['name']);
-        }
         
     }
 
