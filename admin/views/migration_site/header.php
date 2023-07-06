ef4ab26e9207472b85eb17e914e3fd43200685a5
kobkob2018 <yacov.avr@gmail.com>
2023-07-06 04:39:48 +0300

    ok

--- admin/views/migration_site/header.php
+++ admin/views/migration_site/header.php
@@ -3,5 +3,10 @@
             <a href = "<?= inner_url('migration_site/list/') ?>" class="item-edit-a <?= $view->a_c_class('migration_site') ?>">פרטי האתר לייבוא</a>
             |
             <a href = "<?= inner_url('migration_page/list/') ?>" class="item-edit-a <?= $view->a_c_class('migration_page') ?>">רשימת דפים לייבוא</a>
+            |
+            <a href = "<?= inner_url('migration_cat/list/') ?>" class="item-edit-a <?= $view->a_c_class('migration_cat') ?>">התאמת קטגוריות</a>
+            
+            
+            
     </div>
 </div>
