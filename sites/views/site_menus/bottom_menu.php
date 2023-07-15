3242e0d643fc4d7f6b3f20fb4d6f88d046e75283
kobkob2018 <yacov.avr@gmail.com>
2023-07-15 23:26:30 +0300

    ok

--- sites/views/site_menus/bottom_menu.php
+++ sites/views/site_menus/bottom_menu.php
@@ -20,6 +20,15 @@
                             <?php foreach($menu_item['children'] as $sub_item): ?>
                                 <li class="sub-item <?= $sub_item['css_class'] ?>" id = "sub_item_<?= $sub_item['id'] ?>">
                                     <a href="<?= $sub_item['final_url'] ?>" <?php if($menu_item['target']): ?> target="_BLANK" <?php endif; ?> title="<?= $sub_item['label'] ?>" class="sub-spn sub-a a-link"><?= $sub_item['label'] ?></a>
+                                        <?php if(isset($sub_item['children'])): ?>
+                                            <ul class="sub-sub-menu" id = "sub_sub_menu_<?= $sub_item['id'] ?>">
+                                                <?php foreach($sub_item['children'] as $sub_sub_item): ?>
+                                                    <li class="sub-sub-item <?= $sub_sub_item['css_class'] ?>" id = "sub_sub_item_<?= $sub_sub_item['id'] ?>">
+                                                        <a href="<?= $sub_sub_item['final_url'] ?>" <?php if($sub_sub_item['target']): ?> target="_BLANK" <?php endif; ?> title="<?= $sub_sub_item['label'] ?>" class="sub-spn sub-a a-link"><?= $sub_sub_item['label'] ?></a>
+                                                    </li>
+                                                <?php endforeach; ?>
+                                            </ul>
+                                        <?php endif; ?>
                                 </li>
                             <?php endforeach; ?>
                         </ul>
