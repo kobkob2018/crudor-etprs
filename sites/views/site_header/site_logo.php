248c998af0d24eaae07bace9e7d30542669a30ba
kobkob2018 <yacov.avr@gmail.com>
2023-07-27 19:19:17 +0300

    ok

--- sites/views/site_header/site_logo.php
+++ sites/views/site_header/site_logo.php
@@ -1,3 +1,3 @@
 <a href="<?= outer_url() ?>" title = "<?= $this->data['site']['title'] ?>">
-    <img src="<?= $this->file_url_of('logo',$this->data['site']['logo']) ?>" alt="<?= $this->data['site']['title']; ?>" />
+    <img src="<?= $this->file_url_of('logo',$this->data['site']['logo']) ?>?v=<?= get_config("cash_version") ?>" alt="<?= $this->data['site']['title']; ?>" />
 </a>
