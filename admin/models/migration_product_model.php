9210e83a36d0e7d84ea909aa597de1315c13ec26
kobkob2018 <yacov.avr@gmail.com>
2023-07-31 16:20:12 +0300

    ok

--- admin/models/migration_product_model.php
+++ admin/models/migration_product_model.php
@@ -235,6 +235,7 @@
             $new_product = array(
                 'label'=>utgt($product['name']),
                 'title'=>utgt($product['name']),
+                'list_label'=>utgt($product['name']),
                 'site_id'=>$site_id,
                 'active'=>($product['active'] == '0')? '1' : '0',
                 'priority'=>$product['place']
