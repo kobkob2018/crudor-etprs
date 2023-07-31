b938af226a9d35aba647065706fe7de5d7f51f9a
kobkob2018 <yacov.avr@gmail.com>
2023-07-31 16:04:34 +0300

    ok

--- admin/views/migration_product/prepare.php
+++ admin/views/migration_product/prepare.php
@@ -3,14 +3,14 @@
 <?php if($info['migration_exist']): ?>
     <div class="focus-box red">
         כבר קיימים מוצרים מיובאים באתר זה. על מנת לייבא, יש למחוק את הייבוא הקודם.
-        <a class="button-focus" href="<?= inner_url('migration_gallery/delete_older/') ?>">
+        <a class="button-focus" href="<?= inner_url('migration_product/delete_older/') ?>">
             לחץ כאן למחיקת ייבוא גלריות ישן
         </a>
     </div>
 <?php else: ?>
     <div class="focus-box red">
         <h4>תהליך הייבוא קורה במכה אחת. וייבא את כל המוצרים, התמונות והקטגוריות מאתר המקור. לאחר התהליך יש לוודא את תקינות הייבוא</h4>
-        <a class="button-focus" href="<?= inner_url('migration_gallery/do_migrate/') ?>">
+        <a class="button-focus" href="<?= inner_url('migration_product/do_migrate/') ?>">
             לחץ כאן להתחלת ייבוא
         </a>
     </div>
