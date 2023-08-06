a1e8c7c8e47f6bd7cff55660d465e892feceb064
kobkob2018 <yacov.avr@gmail.com>
2023-08-06 16:25:48 +0300

    ok

--- master_admin/views/migration_user_payments/prepare.php
+++ master_admin/views/migration_user_payments/prepare.php
@@ -3,14 +3,14 @@
 <?php if($info['migration_exist']): ?>
     <div class="focus-box red">
         כבר קיימים תשלומים מיובאים למשתמש זה. על מנת לייבא, יש למחוק את הייבוא הקודם.
-        <a class="button-focus" href="<?= inner_url('migration_user_payments/delete_older/') ?>">
+        <a class="button-focus" href="<?= inner_url('migration_user_payments/delete_older/?user_id='.$_REQUEST['user_id']) ?>">
             לחץ כאן למחיקת ייבוא מוצרים ישן
         </a>
     </div>
 <?php else: ?>
     <div class="focus-box red">
         <h4>תהליך הייבוא קורה במכה אחת. וייבא את כל המוצרים, התמונות והקטגוריות מאתר המקור. לאחר התהליך יש לוודא את תקינות הייבוא</h4>
-        <a class="button-focus" href="<?= inner_url('migration_user_payments/do_migrate/') ?>">
+        <a class="button-focus" href="<?= inner_url('migration_user_payments/do_migrate/?user_id='.$_REQUEST['user_id']) ?>">
             לחץ כאן להתחלת ייבוא
         </a>
     </div>
