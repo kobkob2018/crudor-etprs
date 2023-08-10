56b4c1f36a7544f40b68693c84e6603b2e9edb4c
kobkob2018 <yacov.avr@gmail.com>
2023-08-10 10:32:27 +0300

    ok

--- sites/views/net_banners/leftbar_net_banners.php
+++ sites/views/net_banners/leftbar_net_banners.php
@@ -4,7 +4,7 @@
         <img src="<?= inner_url("banner_count/views/") ?>?banner_id=<?= $banner['id'] ?>" style="display:none" />
         <?php if($banner['video'] != ''): ?>
             <a href = "javascript://" class="banner-clicker" data-link="<?= add_url_params($banner['goto_href'],array('banner_id'=>$banner['id'])) ?>" data-count_url="<?= inner_url("banner_count/clicks/") ?>?banner_id=<?= $banner['id'] ?>">
-                <video class='cube-banner-vid' width="100%" controls="" autoplay loop muted playsinline <?php if($banner['image'] != ""): ?> poster="<?= $this->file_master_url_of('net_banners', $banner['image']) ?>" <?php endif; ?>>
+                <video class='cube-banner-vid' width="100%" autoplay loop muted="" <?php if($banner['image'] != ""): ?> poster="<?= $this->file_master_url_of('net_banners', $banner['image']) ?>" <?php endif; ?>>
                     <source src="<?= $this->file_master_url_of('net_banners', $banner['video']) ?>" alt="<?= $banner['label'] ?>" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
