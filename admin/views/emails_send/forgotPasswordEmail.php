35cc5dbd0c265e2b8467723239e85bf307ee7cdf
kobkob2018 <yacov.avr@gmail.com>
2023-09-06 14:04:51 +0300

    ok

--- admin/views/emails_send/forgotPasswordEmail.php
+++ admin/views/emails_send/forgotPasswordEmail.php
@@ -6,9 +6,16 @@
 	לצורך איפוס הסיסמה, יש ללחוץ על הלינק המצורף, ואז בדף שנפתח לבחור סיסמה חדשה.
 	<br/>
 	לאחר איפוס הסיסמה יש להכנס שוב למערכת עם שם המשתמש שלך והסיסמה שהחרת.
+
+	<br/>
+	<br/>
+
 	שם המשתמש שלך: <br/>
 	<?= $this->data['forgot_password_user']['username']; ?>
-	לבקשתך, נשלח אליך לינק לאיפוס סיסמה:
+
+	<br/>
+	<br/>
+	להלן לינק לאיפוס סיסמה:
 	<a href="<?= outer_url('userLogin/resetPassword/'); ?>?row_id=<?= $this->data['forgot_password_token']['row_id'] ?>&token=<?= $this->data['forgot_password_token']['token'] ?>">לחץ כאן לאיפוס סיסמה</a> 
 
 </div>
