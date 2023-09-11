2698fe6fe9f86b5234d85fb1405582422538c698
kobkob2018 <yacov.avr@gmail.com>
2023-09-11 14:22:58 +0300

    ok

--- admin/modules/user_sites_module.php
+++ admin/modules/user_sites_module.php
@@ -4,9 +4,13 @@
         public $add_models = array("sites");
 		
         public function raff_list(){
+            $user_sites_list = Sites::get_user_site_list();
+            $user_sites_by_index = Helper::eazy_index_arr_by('id',$user_sites_list);
             $info = array(
-                'site_list_type'=>'normal'
+                'site_list_type'=>'normal',
+                'user_sites_by_id'=>$user_sites_by_index
             );
+
             if(isset($_REQUEST['master_list'])){
                 if(Helper::user_is('master_admin',$this->user)){
                     $sites_list = Sites::get_list();
@@ -18,7 +22,7 @@
                 }
             }
             else{
-                $sites_list = Sites::get_user_site_list();
+                $sites_list = $user_sites_list;
             }
 
             $this->add_data('user_sites_link_list',$sites_list? $sites_list: array());
