a92992dd9f235fcc6e1c27a85053e98a80957011
kobkob2018 <yacov.avr@gmail.com>
2023-09-18 11:01:29 +0300

    ok

--- admin/views/migration_cat/list.php
+++ admin/views/migration_cat/list.php
@@ -19,7 +19,7 @@
                 <div class="col">כפתור שיוך</div>
             </div>
             <?php foreach($this->data['current_cat_list'] as $cat): ?>
-                <div class="new-cat table-tr row active-0<?= $cat['active'] ?> is-visible-0<?= $cat['visible'] ?> deep-0<?=  $cat['deep'] ?>">
+                <div class="new-cat awaiting-children-current table-tr row active-0<?= $cat['active'] ?> is-visible-0<?= $cat['visible'] ?> deep-0<?=  $cat['deep'] ?>" data-cat_id="<?= $cat['id'] ?>">
                     <div class="col col-tiny">
                         <?= $cat['id'] ?>
                         <?php if($cat['visible'] == '0'): ?>
@@ -37,13 +37,8 @@
                     <div class="col col-tiny">
                         <?= $cat['active'] ?>
                     </div>
-                    <div class="col pairs-col">
-                        <?php foreach($cat['pairs'] as $cat_pair): ?> 
-                        <small class="new-cat-pair pair-label cat-pair-for-old-<?= $cat_pair['old_cat_id'] ?>">
-                            <a class="pair-x" href="javascript://" onclick="pair_remove(this)" data-old_cat_id="<?= $cat_pair['old_cat_id'] ?>" >X</a>
-                           <?= $cat_pair['label'] ?>
-                        </small>
-                        <?php endforeach; ?>
+                    <div class="col pairs-col awaiting-pairs" data-cat_id="<?= $cat['id'] ?>">
+
                     </div>
                     <div class="col col-tiny">
 
@@ -72,7 +67,7 @@
                 <div class="col">תיאום</div>
             </div>
             <?php foreach($this->data['migrate_cat_list'] as $cat): ?>
-                <div class="old-cat old-cat-<?= $cat['id'] ?> table-tr row cat_status-0<?= $cat['status'] ?>  is-hidden-0<?= $cat['hidden'] ?> deep-0<?=  $cat['deep'] ?> has-ad-0<?= $cat['googleADSense'] == '' ? '0': '1' ?>">
+                <div class="old-cat awaiting-children-old old-cat-<?= $cat['id'] ?> table-tr row cat_status-0<?= $cat['status'] ?>  is-hidden-0<?= $cat['hidden'] ?> deep-0<?=  $cat['deep'] ?> has-ad-0<?= $cat['googleADSense'] == '' ? '0': '1' ?>" data-cat_id="<?= $cat['id'] ?>">
                     <div class="col col-tiny">
                         <a class="pair-button" href="javascript://" onclick="pair_cat_go(this)" data-cat_id="<?= $cat['id'] ?>">
                         <<-
