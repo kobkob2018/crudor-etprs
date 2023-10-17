5f2483837fcf7963e22b39fb46e26d1f0bd791cb
kobkob2018 <yacov.avr@gmail.com>
2023-10-17 12:41:50 +0300

    ok

--- sites/modules/products_module.php
+++ sites/modules/products_module.php
@@ -4,7 +4,16 @@
         public function cubes(){
             $this->controller->add_asset_mapping(SiteProducts::$assets_mapping);
             $site_id = $this->controller->data['site']['id'];
-            $product_list = siteProducts::get_product_list($site_id, false, false, false, "RAND()");
+            $action_data = $this->decode_action_data_arr();
+            $limit = '4';
+            if(isset($action_data['limit'])){
+                $limit = trim($action_data['limit']);
+            }
+            $cat_id = false;
+            if(isset($action_data['cat'])){
+                $cat_id = trim($action_data['cat']);
+            }
+            $product_list = siteProducts::get_product_list($site_id, $cat_id, false, $limit, "RAND()");
             if(!$product_list){
                 return;
             }
