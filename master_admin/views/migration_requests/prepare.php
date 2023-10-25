afd6cf4f6ad7bdf5f6ef26bdcdb9724056f41532
kobkob2018 <yacov.avr@gmail.com>
2023-10-25 12:30:14 +0300

    Ok

--- master_admin/views/migration_requests/prepare.php
+++ master_admin/views/migration_requests/prepare.php
@@ -1,12 +1,12 @@
 <h2>ייבוא לידים</h2>
-<?php if($info['migration_exist']): ?>
+
     <div class="focus-box red">
-        כבר קיימות גלריות מיובאות באתר זה.ניתן למחוק את הייבוא הקודם.
+        אם קיים -ניתן למחוק את הייבוא הקודם.
         <a class="button-focus" href="<?= inner_url('migration_requests/delete_older/') ?>">
             לחץ כאן למחיקת ייבוא לידים ישן
         </a>
     </div>
-<?php endif; ?>
+
 
 <div class="focus-box red">
     <h2>ייבוא לידים</h2>
