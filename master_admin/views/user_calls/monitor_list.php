73c53ab507774fadaab9a6e157103090d9102073
kobkob2018 <yacov.avr@gmail.com>
2023-11-09 11:11:48 +0200

    ok

--- master_admin/views/user_calls/monitor_list.php
+++ master_admin/views/user_calls/monitor_list.php
@@ -2,7 +2,7 @@
     <?php foreach($info['phone_list'] as $phone_data): ?>
         <tr  class="phone_list_row" data-state="phone" data-uniqid="<?php echo $phone_data['id']; ?>" >
             <td class="current_call_td_userid hide"><?php echo $phone_data['user_id']; ?></td>
-            <td class="current_call_td_from"><a target="new" href="<?= get_config('master_url') ?>/?aff_id=18&custom_phone=<?php echo $phone_data['call_from']; ?>&custom_cat=<?php echo $phone_data['custom_cat']; ?>&link_uniq=<?php echo $phone_data['id']; ?>"><?php echo $phone_data['call_from']; ?></a></td>
+            <td class="current_call_td_from"><a target="_BLANK" href="<?= get_config('master_url') ?>/?aff_id=18&custom_phone=<?php echo $phone_data['call_from']; ?>&custom_cat=<?php echo $phone_data['custom_cat']; ?>&link_uniq=<?php echo $phone_data['id']; ?>"><?php echo $phone_data['call_from']; ?></a></td>
             <td class="current_call_td_to"><?php echo $phone_data['call_to']; ?></td>
             <td class="current_call_td_did hide"><?php echo $phone_data['did']; ?></td>
             <td class="current_call_td_date hide"><?php echo $phone_data['call_date']; ?></td>
