c3958569fd68390adb695c27d58762a97bdec858
kobkob2018 <yacov.avr@gmail.com>
2024-01-22 11:15:39 +0200

    ok

--- master_admin/controllers/meta_webhooks_controller.php
+++ master_admin/controllers/meta_webhooks_controller.php
@@ -2,7 +2,7 @@
 class Meta_webhooksController extends CrudController{
 //this is the controller who recives calls sent directly from link, at real time
     protected function handle_access($action){
-
+        return true;
         if(isset($_REQUEST['token']) && $_REQUEST['token'] == 'kobkob'){
             Helper::add_log('meta_webhooks_no_token.txt',"\n\n\n: ".date("m/d/Y H:i", time())."-kobkob");
             exit("fail");
@@ -15,14 +15,14 @@ class Meta_webhooksController extends CrudController{
         return false;
     }
 
-    public function ajax_msg_recived(){
-        $this->set_layout('blank');
+    public function msg_recived(){
+        
         $request_smg = "";
         foreach($_REQUEST as $key=>$val){
             $request_smg = "\n$key: $val";
         }
         Helper::add_log('meta_webhooks.txt',"\n\n\n: ".date("m/d/Y H:i", time()).":$request_smg");
-        exit("ok");
+        
     }
 }
 ?>
