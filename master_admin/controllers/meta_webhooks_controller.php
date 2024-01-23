a75c1a20b5f96fe05593b45881c8fe5ec6f796e2
kobkob2018 <yacov.avr@gmail.com>
2024-01-23 18:28:55 +0200

    ok

--- master_admin/controllers/meta_webhooks_controller.php
+++ master_admin/controllers/meta_webhooks_controller.php
@@ -6,7 +6,7 @@ class Meta_webhooksController extends CrudController{
         
         $request_smg = "";
         foreach($_REQUEST as $key=>$val){
-            $request_smg = "\n$key: $val";
+            $request_smg .= "\n$key: $val";
         }
         Helper::add_log('meta_webhooks.txt',"\n\n\n: ".date("m/d/Y H:i", time()).":$request_smg");
         exit("1417343539");
