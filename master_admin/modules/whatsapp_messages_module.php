909fd0c0c47f04abbd1bc46631d65e9a5e4e7010
kobkob2018 <yacov.avr@gmail.com>
2024-02-13 14:15:22 +0200

    ok

--- master_admin/modules/whatsapp_messages_module.php
+++ master_admin/modules/whatsapp_messages_module.php
@@ -97,8 +97,8 @@
 
         $message_data = $this->action_data;
         $message_info = json_decode($message_data['message_info'],true);
-
-        Helper::add_log('meta_webhooks.txt',"\n\n\n: ".date("m/d/Y H:i", time()).":".$message_data['message_info']);
+        Helper::add_log('meta_webhooks.txt',"stam hereee");
+       // Helper::add_log('meta_webhooks.txt',"\n\n\n: ".date("m/d/Y H:i", time()).":".$message_data['message_info']);
         if(
             (!isset($message_info['entry'])) ||
             (!isset($message_info['entry']['changes'])) ||
