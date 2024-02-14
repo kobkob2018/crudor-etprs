8953d0d63df6f2e3e43f076b7dae7b85a5beae4c
kobkob2018 <yacov.avr@gmail.com>
2024-02-14 10:09:19 +0200

    ok

--- master_admin/views/whatsapp_conversations/list.php
+++ master_admin/views/whatsapp_conversations/list.php
@@ -11,25 +11,20 @@
         <div class="col">
             עדכון שיחה
         </div>
-        <div class="col">חשבון</div>
-        <div class="col">פונה</div>
         <div class="col">שם ווטסאפ</div>
         <div class="col">שם הפונה</div>
-        <div class="col">פנייה אחרונה</div>
-        <div class="col">כיוון</div>
-        <div class="col">זמן</div>
         <div class="col">מחיקה</div>
     </div>
     <?php foreach($this->data['whatsapp_conversations'] as $item): ?>
         <div class="table-tr row">
             <div class="col"><?= $item['id'] ?></div>
-            <div class="col"><?= hebdt($item['last_message_time']) ?></div>
+            <div class="col"><?= hebdt($item['last_message_time'],'H:i<br/>d-m-Y') ?></div>
             <div class="col"><?= $item['contact_phone_wa_id'] ?></div>
             <div class="col">
                 <a href = "<?= inner_url('whatsapp_conversations/edit/') ?>?&row_id=<?= $item['id'] ?>" title="ערוך באנר"><?= $item['contact_wa_name'] ?></a>
             </div>
             <div class="col"><?= $item['contact_custom_name'] ?></div>
-            <div class="col"><?= $item['last_message']['message_type'] ?>: <?= $item['last_message']['message_text'] ?></div>
+            <div class="col"><b><?= $item['direction'] ?></b><br/><?= $item['last_message']['message_type'] ?>: <?= $item['last_message']['message_text'] ?></div>
             <div class="col">
                 <a href = "<?= inner_url('whatsapp_conversations/delete/') ?>?row_id=<?= $item['id'] ?>" title="מחק">מחק</a>
             </div>
