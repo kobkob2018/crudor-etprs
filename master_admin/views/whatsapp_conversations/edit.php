4a452267a219756ecdf0779cee013a60b426a760
kobkob2018 <yacov.avr@gmail.com>
2024-02-14 10:24:20 +0200

    Ok

--- master_admin/views/whatsapp_conversations/edit.php
+++ master_admin/views/whatsapp_conversations/edit.php
@@ -4,7 +4,8 @@
     </div>
     <h2>עריכת איש קשר בווטסאפ</h2>
     <div class="item-info focus-box">
-        <b><?php print_r_help($this->data); ?></b>
+        <b>טלפון ווטסאפ: </b><?= $this->data['item_info']['contact_phone_wa_id'] ?>
+        <b>שם ווטסאפ: </b><?= $this->data['item_info']['contact_wa_name'] ?>
     </div>
     <div id="block_form_wrap" class="form-gen page-form">
         <?php $this->include_view('form_builder/form.php'); ?>
