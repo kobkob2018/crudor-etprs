43a0ae8225cd28f8b8e139f799523d019cca8d2a
kobkob2018 <yacov.avr@gmail.com>
2024-02-14 09:53:00 +0200

    ok

--- master_admin/views/whatsapp_conversations/list.php
+++ master_admin/views/whatsapp_conversations/list.php
@@ -19,6 +19,38 @@
         <div class="col">זמן</div>
         <div class="col">מחיקה</div>
     </div>
-    
+    <?php foreach($this->data['net_banners'] as $banner): ?>
+        <div class="table-tr row">
+            <div class="col">
+                <a href = "<?= inner_url('net_banners/edit/') ?>?dir_id=<?= $this->data['dir_info']['id'] ?>&row_id=<?= $banner['id'] ?>" title="ערוך באנר"><?= $banner['label'] ?></a>
+                <hr/>
+                <div class="col-table-30">
+                    <div class="col-30">
+                        <?= $banner['views'] ?>
+                    </div>
+                    <div class="col-30">
+                        <?= $banner['clicks'] ?>
+                        <?php if($banner['views']): ?>
+                        <br/>
+                            <?= round(intval($banner['clicks'])/intval($banner['views'])*100) ?>%
+                        <?php endif; ?>
+                    </div>
+                    <div class="col-30">
+                        <?= $banner['convertions'] ?>
+                        <?php if($banner['clicks']): ?>
+                            <br/>
+                            <?= round(intval($banner['convertions'])/intval($banner['clicks'])*100) ?>%
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <?= $banner['active_str'] ?>
+            </div>
+            <div class="col">
+                <a href = "<?= inner_url('net_banners/delete/') ?>?dir_id=<?= $this->data['dir_info']['id'] ?>&row_id=<?= $banner['id'] ?>" title="מחק">מחק</a>
+            </div>
+        </div>
+    <?php endforeach; ?>
 </div>
 
