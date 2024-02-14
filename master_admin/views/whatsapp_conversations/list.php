461a254dba91ffaa44cf4d815726e5aad63116c6
kobkob2018 <yacov.avr@gmail.com>
2024-02-14 10:03:23 +0200

    ok

--- master_admin/views/whatsapp_conversations/list.php
+++ master_admin/views/whatsapp_conversations/list.php
@@ -7,6 +7,7 @@
 <div class="items-table flex-table">
     <div class="table-th row">
         <div class="col">#</div>
+        <div class="col"></div>
         <div class="col">
             עדכון שיחה
         </div>
@@ -19,36 +20,18 @@
         <div class="col">זמן</div>
         <div class="col">מחיקה</div>
     </div>
-    <?php foreach($this->data['net_banners'] as $banner): ?>
+    <?php foreach($this->data['whatsapp_conversations'] as $item): ?>
         <div class="table-tr row">
+            <div class="col"><?= $item['id'] ?></div>
+            <div class="col"><?= hebdt($item['last_message_time']) ?></div>
+            <div class="col"><?= $item['contact_phone_wa_id'] ?></div>
             <div class="col">
-                <a href = "<?= inner_url('net_banners/edit/') ?>?dir_id=<?= $this->data['dir_info']['id'] ?>&row_id=<?= $banner['id'] ?>" title="ערוך באנר"><?= $banner['label'] ?></a>
-                <hr/>
-                <div class="col-table-30">
-                    <div class="col-30">
-                        <?= $banner['views'] ?>
-                    </div>
-                    <div class="col-30">
-                        <?= $banner['clicks'] ?>
-                        <?php if($banner['views']): ?>
-                        <br/>
-                            <?= round(intval($banner['clicks'])/intval($banner['views'])*100) ?>%
-                        <?php endif; ?>
-                    </div>
-                    <div class="col-30">
-                        <?= $banner['convertions'] ?>
-                        <?php if($banner['clicks']): ?>
-                            <br/>
-                            <?= round(intval($banner['convertions'])/intval($banner['clicks'])*100) ?>%
-                        <?php endif; ?>
-                    </div>
-                </div>
+                <a href = "<?= inner_url('whatsapp_conversations/edit/') ?>?&row_id=<?= $item['id'] ?>" title="ערוך באנר"><?= $item['contact_wa_name'] ?></a>
             </div>
+            <div class="col"><?= $item['contact_custom_name'] ?></div>
+            <div class="col"><?= $item['last_message']['message_type'] ?>: <?= $item['last_message']['message_text'] ?></div>
             <div class="col">
-                <?= $banner['active_str'] ?>
-            </div>
-            <div class="col">
-                <a href = "<?= inner_url('net_banners/delete/') ?>?dir_id=<?= $this->data['dir_info']['id'] ?>&row_id=<?= $banner['id'] ?>" title="מחק">מחק</a>
+                <a href = "<?= inner_url('whatsapp_conversations/delete/') ?>?row_id=<?= $item['id'] ?>" title="מחק">מחק</a>
             </div>
         </div>
     <?php endforeach; ?>
