c6d659816d373d129cee21e3faf1b033ed95f5e6
kobkob2018 <yacov.avr@gmail.com>
2024-02-14 10:20:47 +0200

    ok

--- master_admin/views/whatsapp_conversations/edit.php
+++ master_admin/views/whatsapp_conversations/edit.php
@@ -2,6 +2,10 @@
     <div class="eject-box">
         <a href="<?= $this->eject_url() ?>">חזרה לרשימת השיחות</a>
     </div>
+    <h2>עריכת איש קשר בווטסאפ</h2>
+    <div class="item-info focus-box">
+        <b><?php print_r_help($this->data); ?></b>
+    </div>
     <div id="block_form_wrap" class="form-gen page-form">
         <?php $this->include_view('form_builder/form.php'); ?>
     </div>
