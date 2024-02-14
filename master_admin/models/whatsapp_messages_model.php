40b8a6d7a0e76945e43016fb6c46a64f52c1fb7a
kobkob2018 <yacov.avr@gmail.com>
2024-02-14 13:03:33 +0200

    ok

--- master_admin/models/whatsapp_messages_model.php
+++ master_admin/models/whatsapp_messages_model.php
@@ -1,6 +1,24 @@
 <?php 
     class Whatsapp_messages extends TableModel{
         protected static $main_table = 'whatsapp_messages';  
+
+        public static $fields_collection = array(
+            'campaign_type'=>array(
+                'label'=>'סוג המסר',
+                'type'=>'select',
+                'default'=>'text',
+                'options'=>array(
+                    array('value'=>'text', 'title'=>'טקסט רגיל'),
+                    array('value'=>'template', 'title'=>'תבנית')
+                )
+            ),
+            'messages_text'=>array(
+                'label'=>'טקסט \ תבנית',
+                'type'=>'text',
+            ),
+        );
     }
+
+    
 
 ?>
