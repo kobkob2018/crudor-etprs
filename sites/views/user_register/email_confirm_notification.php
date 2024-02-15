8c4d556e7f1f8efe65041b3999d97d5e47415a7a
kobkob2018 <yacov.avr@gmail.com>
2024-02-15 10:31:25 +0200

    ok

--- sites/views/user_register/email_confirm_notification.php
+++ sites/views/user_register/email_confirm_notification.php
@@ -4,7 +4,7 @@
 	<h3><?= __tr("Your registration to the website $1 succided",array($info['site']['title'])) ?></h3>
 	<br/><br/>
 	<?= __tr("Click the following link to approve your email") ?>:
-    <a href="<?= $info['confirm_url'] ?>" title="לחץ כאן לאישור כתובת המייל"><?= $info['confirm_url'] ?></a>
+    <a href="<?= $info['confirm_url'] ?>" title="<?= __tr("Click here for email confirmation") ?>"><?= $info['confirm_url'] ?></a>
 
     <?php $this->include_view("emails_send/email_footer.php"); ?>
 
