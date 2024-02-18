0601d14a6822ce03d10507e4db7bd8c3f55b8d9d
kobkob2018 <yacov.avr@gmail.com>
2024-02-18 16:28:19 +0200

    ok

--- sites/views/modules/whatsapp_chat.php
+++ sites/views/modules/whatsapp_chat.php
@@ -1,5 +1,5 @@
 <div class="whatsapp-chat-wrap">
-    <a href="whatsapp://send?text=<?= $info['message'] ?>?&phone=<?= $info['phone'] ?>">
+    <a href="whatsapp://send?text=<?= $info['message'] ?>&phone=<?= $info['phone'] ?>">
         <img src="<?= $info['image'] ?>" alt="<?= __tr("Contact with Whatsapp") ?>" />
     </a>
 </div>
