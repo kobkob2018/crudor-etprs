971c39940abfffe4bea188c6c5ddac83e6f712ac
kobkob2018 <yacov.avr@gmail.com>
2024-02-20 12:05:24 +0200

    ok

--- master_admin/modules/whatsapp_messages_module.php
+++ master_admin/modules/whatsapp_messages_module.php
@@ -222,6 +222,7 @@
         $message_text .= "message: \n";
         $message_text .= $message_row_data['message_type'].": \n".$message_row_data['message_text'];
         $message_text .= "\n\nview in admin: ".outer_url('whatsapp_messages/add/?conversation_id='.$conversation_data['id']);
+        Helper::add_log('meta_webhooks_admin.txt',"\n\n\n $message_text");
         $message_data = array(
             'message_type'=>'text',
             'message_text'=>$message_text,
