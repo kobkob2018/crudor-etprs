7e5875fb02aea6686bec3dfc9437bb31bfba3be8
kobkob2018 <yacov.avr@gmail.com>
2024-03-10 11:48:06 +0200

    ok

--- master_admin/models/link_system_calls_model.php
+++ master_admin/models/link_system_calls_model.php
@@ -287,6 +287,7 @@
     }
 
     protected function add_972_needle_sql_arr($phone){
+      Helper::add_log('needle_phones.txt',"\nchecking phone".$phone,"\n\n");
       $needle_phone = self::replace_needle_with_0($phone,'972');
       if($needle_phone == $phone){
         $needle_phone = self::replace_needle_with_0($phone,'+972');
