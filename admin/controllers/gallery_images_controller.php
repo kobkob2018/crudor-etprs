5ad60f8d730d80b81102cc0eac494b890c6d007f
kobkob2018 <yacov.avr@gmail.com>
2024-03-12 13:06:51 +0200

    ok

--- admin/controllers/gallery_images_controller.php
+++ admin/controllers/gallery_images_controller.php
@@ -37,6 +37,7 @@
           $filter_arr['user_id'] = $this->user['id'];
         }
         $list_info = $this->get_paginated_list_info($filter_arr);
+        print_r_help($list_info);
         if($user_is_admin){
             $users_by_id = array();
             foreach($list_info['list'] as $key=>$page){
