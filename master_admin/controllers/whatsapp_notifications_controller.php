bb4b94d3d69413105015b5904843788789dc810f
kobkob2018 <yacov.avr@gmail.com>
2024-03-18 13:17:15 +0200

    ok

--- master_admin/controllers/whatsapp_notifications_controller.php
+++ master_admin/controllers/whatsapp_notifications_controller.php
@@ -74,11 +74,11 @@
     }
 
     public function include_edit_view(){
-        $this->include_view('whatsapp_conversations/edit.php');
+        $this->include_view('whatsapp_notifications/edit.php');
     }
 
     public function include_add_view(){
-        $this->include_view('whatsapp_conversations/add.php');
+        $this->include_view('whatsapp_notifications/add.php');
     }   
 
     protected function update_success_message(){
@@ -100,35 +100,35 @@
     }   
 
     protected function delete_item($row_id){
-      return Whatsapp_conversations::delete($row_id);
+      return Whatsapp_notifications::delete($row_id);
     }
 
     protected function get_item_info($row_id){
-      return Whatsapp_conversations::get_by_id($row_id);
+      return Whatsapp_notifications::get_by_id($row_id);
     }
 
     public function eject_url(){
-      return inner_url('whatsapp_conversations/list/');
+      return inner_url('whatsapp_notifications/list/');
     }
 
     public function url_back_to_item($item_info){
-      return inner_url("whatsapp_conversations/edit/?row_id=".$item_info['id']);
+      return inner_url("whatsapp_notifications/edit/?row_id=".$item_info['id']);
     }
 
     public function delete_url($item_info){
-        return inner_url("whatsapp_conversations/delete/?row_id=".$item_info['id']);
+        return inner_url("whatsapp_notifications/delete/?row_id=".$item_info['id']);
     }
 
     protected function get_fields_collection(){
-      return Whatsapp_conversations::setup_field_collection();
+      return Whatsapp_notifications::setup_field_collection();
     }
 
     protected function update_item($item_id,$update_values){
-      return Whatsapp_conversations::update($item_id,$update_values);
+      return Whatsapp_notifications::update($item_id,$update_values);
     }
 
     protected function create_item($fixed_values){
-        return Whatsapp_conversations::create($fixed_values);
+        return Whatsapp_notifications::create($fixed_values);
     }
   }
 ?>
