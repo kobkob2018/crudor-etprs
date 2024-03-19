27cc4443f9905d4ee88027df163050f28104f1cf
kobkob2018 <yacov.avr@gmail.com>
2024-03-19 09:01:47 +0200

    ok

--- master_admin/views/whatsapp_notifications/list.php
+++ master_admin/views/whatsapp_notifications/list.php
@@ -25,7 +25,7 @@
         <div class="table-tr row">
             <div class="col col-tiny"><?= $key ?></div>
             <div class="col">
-                <div class="notification_row type_<?= $note['type'] ?>">
+                <div class="notification_row type_<?= $note['type'] ?>" onclick="toggle_notification(this)">
                     <?php foreach($note['values'] as $note_val): ?>
                         <?php $class = str_replace('messages','messages-key',$note_val['class']); ?>
                         <?php $class = str_replace('body','body messages',$class); ?>
@@ -45,9 +45,20 @@
 
 <style type= "text/css">
     .notification_row{font-size: 5px;}
+    .notification_row.bigger{font-size: 16px;}
     .notification_row.type_text .from,
     .notification_row.type_text .body,
     .notification_row.type_text .name,
     .notification_row.type_text .display_phone_number
     {font-size: 18px;}
 </style>
+<script type="text/javascript">
+    function toggle_notification(note_el){
+        if(note_el.classList.contains('bigger')){
+            note_el.classList.remove('bigger');
+        }
+        else{
+            note_el.classList.add('bigger');
+        }
+    }
+</script>
