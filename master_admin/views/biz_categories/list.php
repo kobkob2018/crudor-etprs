27363294c360ebf04975184133389b15ea593185
kobkob2018 <yacov.avr@gmail.com>
2024-04-01 10:50:57 +0300

    ok

--- master_admin/views/biz_categories/list.php
+++ master_admin/views/biz_categories/list.php
@@ -27,7 +27,7 @@
             <div class="col">מחיקה</div>
         </div>
         <?php foreach($this->data['cat_list'] as $cat): ?>
-            <div class="table-tr row  pale-0<?= $cat['visible'] ?>">
+            <div class="table-tr row  pale-0<?= $cat['visible'] ?>  pale-0<?= $cat['googleADSense'] != '' ?>">
                 <div class="col col-tiny">
                     <?= $cat['id'] ?>
                 </div>
@@ -40,6 +40,11 @@
                         <br/>
                         לא נראה באתר
                     <?php endif; ?>
+                    <?php if($cat['googleADSense'] != ''): ?>
+                        <br/>
+                        מכיל פרסומת גוגל
+                    <?php endif; ?>
+                    
                 </div>
                 <div class="col">
                     <?= $cat['unique_phone'] ?>
