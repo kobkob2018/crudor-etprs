e01753c90e948ab9037c8ff61ccf1f7cfd04038e
kobkob2018 <yacov.avr@gmail.com>
2024-04-02 11:10:55 +0300

    ok

---
+++ myleads/views/tags/tags_settings_beckup.php
@@ -0,0 +1,71 @@
+
+<style type="text/css">
+//put this in tags settings
+	.tag-color.tag-color-1{
+		background: #bbbbea;
+	}
+	.tag-color.tag-color-2{
+		background: #bbe6ea;
+	}
+	.tag-color.tag-color-3{
+		background: #bbeada;
+	}
+	.tag-color.tag-color-4{
+		background: #cceabb;
+	}
+	.tag-color.tag-color-5{
+		background: #e3eabb;
+	}
+	.tag-color.tag-color-6{
+		background: #ead0bb;
+	}
+	.tag-color.tag-color-7{
+		background: #eabbcf;
+	}
+	.tag-color.tag-color-8{
+		background: #eabbe3;
+	}
+	.tag-color.tag-color-9{
+		background: #d8bbea;
+	}
+	.tag-color.tag-color-10{
+		background: #bbbeea;
+	}
+</style>
+
+
+
+//put this in main css
+.table-striped>tbody>tr.view_state_1.tag-color-1>td {
+    background: #bbbbea;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-2>td {
+    background: #bbe6ea;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-3>td {
+    background: #bbeada;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-4>td {
+    background: #cceabb;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-5>td {
+    background: #e3eabb;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-6>td {
+    background: #ead0bb;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-7>td {
+    background: #eabbcf;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-8>td {
+    background: #eabbe3;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-9>td {
+    background: #d8bbea;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-10>td {
+    background: #bbbeea;
+}
+.table-striped>tbody>tr.view_state_1.tag-color-11>td {
+    background: #bbbeea;
+}
