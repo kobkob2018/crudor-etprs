7eaf9121214250e087362a84ae9fb443ab09f942
kobkob2018 <yacov.avr@gmail.com>
2024-04-03 15:47:58 +0300

    ok

--- master_admin/views/whatsapp_templates/add.php
+++ master_admin/views/whatsapp_templates/add.php
@@ -1,10 +1,10 @@
-<h2>חדשות האתר</h2>
+<h2>תבניות ווטסאפ</h2>
 
 <div class="focus-box">
     <div class="eject-box">
-        <a href="<?= inner_url("news/list/") ?>">חזרה לרשימה</a>
+        <a href="<?= inner_url("whatsapp_templates/list/") ?>">חזרה לרשימה</a>
     </div>
-    <h3>הוספת חדשה</h3>
+    <h3>הוספת תבנית</h3>
     <div id="block_form_wrap" class="form-gen page-form">
         <?php $this->include_view('form_builder/form.php'); ?>
     </div>
--- master_admin/views/whatsapp_templates/edit.php
+++ master_admin/views/whatsapp_templates/edit.php
@@ -1,11 +1,11 @@
-<h2>חדשות האתר</h2>
+<h2>תבניות ווטסאפ</h2>
 
 <div class="focus-box">
     <div class="eject-box">
-    <a href="<?= inner_url("news/list/") ?>">חזרה לרשימת החדשות</a>
+    <a href="<?= inner_url("whatsapp_templates/list/") ?>">חזרה לרשימת התבניות</a>
     </div>
     <hr/>
-    <h3>עריכת חדשה - <?= $this->data['item_info']['label'] ?></h3>
+    <h3>עריכת תבנית - <?= $this->data['item_info']['label'] ?></h3>
     <div id="block_form_wrap" class="form-gen page-form">
         <?php $this->include_view('form_builder/form.php'); ?>
     </div>
