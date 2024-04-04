18ac67d464cb6eafe72a4055bb4571fa8babc7a7
kobkob2018 <yacov.avr@gmail.com>
2024-04-04 12:34:41 +0300

    ok

--- master_admin/views/whatsapp_messages/ajax_list.php
+++ master_admin/views/whatsapp_messages/ajax_list.php
@@ -1,11 +1,16 @@
 <?php foreach($this->data['whatsapp_messages'] as $item): ?>
-    <div class="table-tr row message_tr" data-message_time = "<?= $item['message_time'] ?>" data-message_id="<?= $item['id'] ?>">
+    <div class="table-tr row message_tr <?= $item['send_state'] ?>" data-message_time = "<?= $item['message_time'] ?>" data-message_id="<?= $item['id'] ?>">
         <div class="col"><?= $item['id'] ?></div>
         <div class="col">
-            <div class="<?= $item['direction'] ?> message-direction">
+            <div class="<?= $item['direction'] ?> message-direction ">
                 <b><?= $item['message_type'] ?></b><br/>
                 <?= $item['message_text'] ?>
             </div>
+            <?php if($item['err_msg'] != ''): ?>
+                <div class = "red err-msg">
+                    <b>שגיאה בשליחת המסר: </b><?= $item['err_msg'] ?>
+                </div>
+            <?php endif; ?>
             <?php if(isset($item['context']) && $item['context'] != '0'): ?>
                 <div class="message-context message-context-pending" data-context="<?= $item['context'] ?>">[-- <?= $item['context'] ?> --]</div>
             <?php endif; ?>
