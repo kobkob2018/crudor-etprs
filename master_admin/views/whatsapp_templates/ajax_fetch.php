cc0cfed325cfd46594b8efb9dd1af4bdefeecaaa
kobkob2018 <yacov.avr@gmail.com>
2024-04-04 20:48:20 +0300

    ok

--- master_admin/views/whatsapp_templates/ajax_fetch.php
+++ master_admin/views/whatsapp_templates/ajax_fetch.php
@@ -1,9 +1,9 @@
 <div class="template-info-holder">
     <?php if($info['template']['header_image'] != ''): ?>
-        <div class="image-info-holder" data-image_url="$info['template']['header_image']" ></div>
+        <div class="image-info-holder" data-image_url="<?= $info['template']['header_image'] ?>" ></div>
     <?php endif; ?>
     <?php if($info['template']['header_video'] != ''): ?>
-        <div class="video-info-holder" data-image_url="$info['template']['header_vidoe']" ></div>
+        <div class="video-info-holder" data-image_url="<?= $info['template']['header_vidoe'] ?>" ></div>
     <?php endif; ?>
     <div class="text-info-holder">
         <?= $info['template']['text'] ?>
