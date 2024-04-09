446ea950e87735a3b52185cf37122843de2c380a
kobkob2018 <yacov.avr@gmail.com>
2024-04-09 09:47:01 +0300

    ok

--- admin/views/layouts/layout.php
+++ admin/views/layouts/layout.php
@@ -10,6 +10,7 @@
 		<link rel="stylesheet" href="<?= styles_url('style/css/admin.css') ?>?v=<?= get_config("cash_version") ?>"  type="text/css" />	
 		<link rel="stylesheet" href="<?= styles_url('style/css/admin_only.css') ?>?v=<?= get_config("cash_version") ?>"  type="text/css" />	
 		<link rel="stylesheet" href="<?= global_url("global_style/css/icons.css") ?>?v=<?= get_config("cash_version") ?>"  type="text/css" />
+		<link rel="stylesheet" href="<?= global_url("global_style/css/admin_global.css") ?>?v=<?= get_config("cash_version") ?>"  type="text/css" />
 		<title><?= $this->data['meta_title'] ?></title>
 		<?php $this->include_view('registered_scripts/head.php'); ?>
 
