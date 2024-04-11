35ec997529275e7cd743bdfdf3f6cccd5d271a38
kobkob2018 <yacov.avr@gmail.com>
2024-04-11 12:40:51 +0300

    ok

--- master_admin/views/users/edit.php
+++ master_admin/views/users/edit.php
@@ -7,10 +7,10 @@
 <a href="<?= inner_url("migration_user/list/") ?>?row_id=<?= $this->data['item_info']['id'] ?>">לחץ כאן לניהול ייבוא פרטי לקוח ממערכת ישנה</a>
 
 <div class="hidden comment-holder">
-    <div class="form-builder-comment" data-for="password-filed">
+    <div class="form-builder-comment short" data-for="password-filed">
         <b>השאר ריק אם אינך מחליף סיסמא</b>    
     </div>
-    <div class="form-builder-comment" data-for="password-confirm-filed">
+    <div class="form-builder-comment short" data-for="password-confirm-filed">
         <b>השאר ריק אם אינך מחליף סיסמא</b>    
     </div>
 </div>
